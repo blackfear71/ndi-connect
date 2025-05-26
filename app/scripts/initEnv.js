4e67ffdacfc549da45ec3ecc94d33a5779d50527
HAMONIC Pierre <Pierre.HAMONIC.externe@groupe.schmidt>
2025-05-26 10:30:55 +0200

    Script npm d'initialisation des fichiers .env

---
+++ app/scripts/initEnv.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+// Liste des fichiers à générer
+// TODO : URL à vérifier x3
+const envFiles = [
+    {
+        path: path.resolve('.env.development'),
+        label: 'Front (dev)',
+        content: `REACT_APP_API_URL=http://localhost:3000/ndi-connect/api
+`
+    },
+    {
+        path: path.resolve('.env.production'),
+        label: 'Front (prod)',
+        content: `REACT_APP_API_URL=/api
+`
+    },
+    {
+        path: path.resolve('../api/.env'),
+        label: 'Back',
+        content: `DB_HOST=localhost
+DB_NAME=ndi_connect_db
+DB_USER=root
+DB_PASSWORD=root
+`
+    }
+];
+
+// Création des fichiers
+envFiles.forEach(({ path: filePath, label, content }) => {
+    createEnvFile(filePath, label, content);
+});
+
+// Fonction de création des fichiers d'environnement
+function createEnvFile(filePath, label, content) {
+    if (fs.existsSync(filePath)) {
+        console.log(`[${label}] ${filePath} existe déjà`);
+    } else {
+        fs.writeFileSync(filePath, content);
+        console.log(`[${label}] ${filePath} créé`);
+    }
+}
