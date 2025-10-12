0d27a5117bc85fad592e5f51dc85905a20b5d662
Pierre <pierre.hamonic71@gmail.com>
2025-10-13 00:13:25 +0200

    Retours PR

--- api/core/helpers/ResponseHelper.php
+++ api/core/helpers/ResponseHelper.php
@@ -2,30 +2,6 @@
 class ResponseHelper
 {
     /**
-     * Gestion du retour en cas de succès
-     */
-    public static function success($data = null, $message = '')
-    {
-        echo json_encode([
-            'status' => 'success',
-            'message' => $message,
-            'data' => $data
-        ]);
-    }
-
-    /**
-     * Gestion du retour en cas de succès
-     */
-    public static function info($data = null, $message = '')
-    {
-        echo json_encode([
-            'status' => 'info',
-            'message' => $message,
-            'data' => $data
-        ]);
-    }
-
-    /**
      * Gestion du retour en cas d'erreur
      */
     public static function error($message, $code = 500, $logMessage = null)
@@ -39,6 +15,42 @@ class ResponseHelper
             'status' => 'error',
             'message' => $message,
             'data' => null
+        ]);
+    }
+
+    /**
+     * Gestion du retour en cas d'info
+     */
+    public static function info($data = null, $message = '')
+    {
+        echo json_encode([
+            'status' => 'info',
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * Gestion du retour en cas de succès
+     */
+    public static function success($data = null, $message = '')
+    {
+        echo json_encode([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * Gestion du retour en cas d'alerte
+     */
+    public static function warning($data = null, $message = '')
+    {
+        echo json_encode([
+            'status' => 'warning',
+            'message' => $message,
+            'data' => $data
         ]);
     }
 }
